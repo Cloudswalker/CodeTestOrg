bc8939f7807c89102c074679e7b6b938be9caa64
Cloudswalker <tutkay@gmail.com>
2025-06-24 20:03:33 +0300

    Refactored Program.cs

--- Application/Program.cs
+++ Application/Program.cs
@@ -12,47 +12,50 @@ namespace LogUsers
         static void Main(string[] args)
         {
             Console.WriteLine("Start");
+
             var serviceCollection = new ServiceCollection();
             ConfigureServices(serviceCollection);
+            using var serviceProvider = serviceCollection.BuildServiceProvider();
 
-            using (var serviceProvider = serviceCollection.BuildServiceProvider())
-            {
-                var logger = serviceProvider.GetRequiredService<ILog>();
+            RunWithFlush(serviceProvider);
+            RunWithoutFlush(serviceProvider);
 
-                //add them all
-                for (int i = 0; i < 15; i++)
-                {
-                    logger.Write("Number with Flush: " + i.ToString());
-                }
-
-                logger.StopWithFlush();
-            }
-
-            Console.WriteLine("Finished part 1...");
-            using (var serviceProvider = serviceCollection.BuildServiceProvider())
-            {
-                ILog logger2 = serviceProvider.GetRequiredService<ILog>();
-
-                var loggingTask = Task.Run(() =>
-                {
-                    for (int i = 50; i > 0; i--)
-                    {
-                        logger2.Write("Number with No flush: " + i.ToString());
-                        Thread.Sleep(20);
-                    }
-                });
-                //wait for a moment to ensure some logs are written
-                Task.Delay(500).Wait();
-                logger2.StopWithoutFlush();
-                //wait for the logging task to finish
-                loggingTask.Wait();
-                Console.WriteLine("Finished Part 2...");
-                Console.WriteLine("Finished writing logs. Press Enter to open Logs Directory.");
-                Console.ReadLine();
-
-                Process.Start("explorer.exe", _logDirectory ?? "C:\\DefaultLogDir");
-            }
+            Console.WriteLine("Finished writing logs. Press Enter to open Logs Directory.");
+            Console.ReadLine();
+            Process.Start("explorer.exe", _logDirectory ?? "C:\\DefaultLogDir");
         }
+
+        private static void RunWithFlush(IServiceProvider provider)
+        {
+            var logger = provider.GetRequiredService<ILog>();
+            for (int i = 0; i < 15; i++)
+            {
+                logger.Write($"Number with Flush: {i}");
+            }
+            logger.StopWithFlush();
+            Console.WriteLine("Finished part 1...");
+        }
+
+        private static void RunWithoutFlush(IServiceProvider provider)
+        {
+            var logger = provider.GetRequiredService<ILog>();
+
+            var loggingTask = Task.Run(() =>
+            {
+                for (int i = 50; i > 0; i--)
+                {
+                    logger.Write($"Number with No flush: {i}");
+                    Thread.Sleep(20);
+                }
+            });
+
+            Task.Delay(500).Wait(); // simulate delay before stopping
+            logger.StopWithoutFlush();
+            loggingTask.Wait();
+
+            Console.WriteLine("Finished part 2...");
+        }
+
 
         private static void ConfigureServices(IServiceCollection services)
         {
